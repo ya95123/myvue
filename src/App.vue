806e696ecb4aa00f16d9b3379ab42c0d9420e121
ya95123 <ya95123@gmail.com>
2020-05-18 10:44:05 +0800

    重打練習一遍

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{text}}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// 要匯出去的資訊
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      text: 'aaa'
+    }
   }
 }
 </script>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<style>
+h1 {
+  color: red
+}
 </style>
